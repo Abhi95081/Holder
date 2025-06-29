86c4f24d38e67f9f5db833c1140d510e585fc3d5
Abhi95081 <abhirou7@gmail.com>
2025-06-29 14:11:29 +0530

    Updated Main screen

--- app/src/main/java/com/example/holder/MainActivity.kt
+++ app/src/main/java/com/example/holder/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.holder
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -16,6 +18,7 @@ import com.example.holder.Navigation.AppNavigation
 import com.example.holder.ui.theme.HolderTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
